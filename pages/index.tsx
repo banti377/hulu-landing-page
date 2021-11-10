9146611ae2b930e65580003095e12db871acc776
banti377 <patelbanti27@yahoo.in>
2021-11-10 20:31:30 +0530

    navbar done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import React from 'react';
 import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <NavBar />
     </div>
   );
 };
