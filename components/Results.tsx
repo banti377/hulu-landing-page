1e8def50e4411266a83e4186963c3ac64b4aa21d
banti377 <patelbanti27@yahoo.in>
2021-11-10 22:02:23 +0530

    landing page completed

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import FlipMove from 'react-flip-move';
+
 import { Result } from '../interfaces';
 import Thumbnail from './Thumbnail';
 
@@ -8,11 +10,11 @@ interface Props {
 
 const Results: FC<Props> = ({ results }) => {
   return (
-    <div>
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result: Result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 };
 
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,10 @@
-import { FC } from 'react';
+import {
+  ForwardedRef,
+  forwardRef,
+  ForwardRefExoticComponent,
+  ReactNode,
+  RefAttributes,
+} from 'react';
 import Image from 'next/image';
 import { ThumbUpIcon } from '@heroicons/react/outline';
 
@@ -7,42 +13,56 @@ import { BASE_URL } from '../utils/constants';
 
 interface Props {
   result: Result;
+  ref: ForwardedRef<HTMLDivElement>;
 }
 
-const Thumbnail: FC<Props> = ({
-  result: {
-    title,
-    poster_path,
-    backdrop_path,
-    overview,
-    original_title,
-    media_type,
-    release_date,
-    first_air_date,
-    vote_count,
-  },
-}) => {
-  return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-      <Image
-        layout="responsive"
-        alt={title}
-        src={`${BASE_URL}${backdrop_path || poster_path}`}
-        height={1080}
-        width={1920}
-      />
-      <div className="p-2">
-        <p className="truncate max-w-md">{overview}</p>
-        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
-          {title || original_title}
-        </h2>
-        <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {media_type} · {first_air_date || release_date} ·{' '}
-          <ThumbUpIcon className="h-5 mx-2" /> {vote_count}
-        </p>
+const Thumbnail: ForwardRefExoticComponent<
+  Pick<Props & { children?: ReactNode }, 'result' | 'children'> &
+    RefAttributes<HTMLDivElement>
+> = forwardRef(
+  (
+    {
+      result: {
+        title,
+        backdrop_path,
+        poster_path,
+        overview,
+        original_title,
+        release_date,
+        first_air_date,
+        media_type,
+        vote_count,
+      },
+    },
+    ref
+  ) => {
+    return (
+      <div
+        ref={ref}
+        className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+      >
+        <Image
+          layout="responsive"
+          alt={title}
+          src={`${BASE_URL}${backdrop_path || poster_path}`}
+          height={1080}
+          width={1920}
+        />
+        <div className="p-2">
+          <p className="truncate max-w-md">{overview}</p>
+          <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+            {title || original_title}
+          </h2>
+          <p className="flex items-center opacity-0 group-hover:opacity-100">
+            {media_type} · {first_air_date || release_date} ·{' '}
+            <ThumbUpIcon className="h-5 mx-2" /> {vote_count}
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Thumbnail;
+
+Thumbnail.displayName = 'Thumbnail';
